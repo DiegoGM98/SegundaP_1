2c215ac7e068d0583a28fb21df003bb8851eaa79
DiegoGM98 <132071916+DiegoGM98@users.noreply.github.com>
2023-11-29 16:57:34 -0300

    Agrego codigo

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,31 @@
+public class Bicicleta {
+    // Atributos
+    private String marca;
+    private String color;
+    private String tipo;
+    private int tamanoRueda;
+    private int numeroVelocidades;
+
+    // Constructor
+    public Bicicleta(String marca, String color, String tipo, int tamanoRueda, int numeroVelocidades) {
+        this.marca = marca;
+        this.color = color;
+        this.tipo = tipo;
+        this.tamanoRueda = tamanoRueda;
+        this.numeroVelocidades = numeroVelocidades;
+    }
+
+    // Métodos
+    public void acelerar() {
+        // código para acelerar
+    }
+
+    public void frenar() {
+        // código para frenar
+    }
+
+    public void cambiarVelocidad(int nuevaVelocidad) {
+        // código para cambiar de velocidad
+    }
+}
+
